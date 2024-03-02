31cefbeb2bb244d8edcc234fe0554e9caf1e554b
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-03-02 05:55:37 +0000

    arrived

---
+++ app/controllers/delivery_controller.rb
@@ -0,0 +1,38 @@
+class DeliveryController < ApplicationController
+  def index
+    @deliveries = Delivery.all
+ 
+    render({ :template => "delivery/index" })
+  end
+ 
+ 
+  def create
+    delivery = Delivery.new
+    delivery.description = params.fetch("query_description")
+    delivery.supposed_to_arrive_on = params.fetch("query_date")
+    delivery.details = params.fetch("query_details")
+    delivery.arrived = false
+
+    if delivery.supposed_to_arrive_on != nil && delivery.description != nil
+      delivery.save
+    end
+ 
+    redirect_to("/")
+  end
+ 
+ 
+  def receive
+    delivery_id = params.fetch(:id)
+    delivery = Delivery.where(id: delivery_id).at(0)
+    delivery.arrived = true
+    delivery.save
+
+    redirect_to("/")
+  end
+ 
+ 
+  def delete
+    redirect_to("/")
+  end
+ end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post("insert_delivery", { :controller => "delivery", :action => "create" })
 
+  post("modify_delivery/:id", controller: "delivery", action: "receive")
+ 
+  get("delivery/delete", controller: "delivery", action: "delete")
+ 
   # Defines the root path route ("/")
   root "delivery#index"
 end
